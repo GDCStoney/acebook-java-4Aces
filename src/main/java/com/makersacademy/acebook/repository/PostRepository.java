73c9a82baedd27742b68fdb8da17d2732f40ee60
GDCStoney <g.stoney@btinternet.com>
2020-11-26 23:54:15 +0000

    90% there :)

--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -2,7 +2,9 @@ package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.Post;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
 
 }
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -15,6 +15,7 @@ class App extends React.Component {
         this.onDelete = this.onDelete.bind(this);
         this.onNavigate = this.onNavigate.bind(this);
         this.updatePageSize = this.updatePageSize.bind(this);
+        this.onCreate = this.onCreate.bind(this);
     }
 
     loadFromServer(pageSize) {
@@ -66,9 +67,31 @@ class App extends React.Component {
         }
     }
 
+    onCreate(newPost) {
+        follow(client, root, ['posts']).then(postCollection => {
+            return client({
+                method: 'POST',
+                path: postCollection.entity._links.self.href,
+                entity: newPost,
+                headers: {'Content-Type': 'application/json'}
+            });
+        }).then(response => {
+            return follow(client, root, [
+                {rel: 'posts', params: {'size': this.state.pageSize}}
+            ]);
+        }).then(response => {
+            if (typeof response.entity._links.last !== "undefined") {
+                this.onNavigate(response.entity._links.last.href);
+            } else {
+                this.onNavigate(response.entity._links.self.href);
+            }
+        });
+    }
+
     render() {
         return (
             <div>
+                <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
                 <PostList posts={this.state.posts}
                     links={this.state.links}
                     pageSize={this.state.pageSize}
@@ -183,6 +206,52 @@ class Post extends React.Component {
                     <button onClick={this.handleDelete}>Del</button>
                 </td>
             </tr>
+        )
+    }
+}
+
+class CreateDialog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const newPost = {};
+        this.props.attributes.forEach(attribute => {
+            newPost[attribute] = ReactDom.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newPost);
+
+        this.props.attributes.forEach(attribute => {
+            ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        window.location = "#";
+    }
+
+    render() {
+        const inputs = this.props.attributes.map(attribute =>
+            <p key={attribute}>
+                <input type="text" placeholder={attribute} ref={attribute} className="field"/>
+            </p>
+        );
+
+        return (
+            <div>
+                <a href="#createPost">Create</a>
+                <div id="CreatePost" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+                        <h2>Create new post</h2>
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
